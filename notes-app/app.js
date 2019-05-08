4adb5acf1b38b14836ded0e484cd3aa9ba603c79
MAGNUSSON Pontus <pontus.magnusson@protonmail.com>
2019-05-08 13:41:14 +0200

    Add read/write functionality

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,4 +4,51 @@ const notes = require('./notes.js')
 
 yargs.version('1.0.0')
 
-console.log(yargs.argv)
+// Create remove command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder:{
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (yargs) => notes.addNote(yargs.title, yargs.body)
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    builder: {
+        title: {
+            describe: 'Title of the note to remove',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    describe: 'Remove a note',
+    handler: (yargs) => notes.removeNote(yargs.title)
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a specific note',
+    handler: () => console.log('Reading the note!')
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: () => console.log('Listing all notes!')
+})
+
+yargs.parse();
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,52 @@
-const getNotes = function ()  {
-    return 'Your notes...'
+const fs = require('fs')
+const chalk = require('chalk')
+
+const fileName = 'notes.json'
+
+const getNotes = () => {
+    return loadNotes()
 }
 
-module.exports = getNotes
+const addNote = (title, body) => {
+    const notes = loadNotes()
+
+    if (notes.some((note) => note.title === title)) {
+        console.log(chalk.red.inverse('Note title taken!'))
+    }
+    else{
+        notes.push({title, body})
+        saveNotes(notes)
+        console.log(chalk.green.inverse('New note added!'))
+    }
+}
+
+const removeNote = (title) => {
+    const notes = loadNotes()
+
+    if(notes.some((note) => note.title === title)){
+        const notesToKeep = notes.filter((note) => note.title !== title)
+        saveNotes(notesToKeep)
+        console.log(chalk.green.inverse('Note removed!'))
+    }
+    else {
+        console.log(chalk.red.inverse('No note found!'))
+    }
+}
+
+const loadNotes = () => {
+    try{
+        return JSON.parse(fs.readFileSync(fileName).toString())
+    } catch(error) {
+        return []
+    }
+}
+
+const saveNotes = (notes) => {
+    fs.writeFileSync(fileName, JSON.stringify(notes))
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote
+}
