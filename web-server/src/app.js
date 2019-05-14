1c2fc89fd119248c78f9bb64aa3a910a63ac3103
MAGNUSSON Pontus <pontus.magnusson@protonmail.com>
2019-05-14 16:39:44 +0200

    Add html and json example returns

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,29 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help page')
+    res.send([{
+        name: 'Pontus',
+        age: 24
+    },{
+        name: 'David',
+        age: 29
+    }
+    ])
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.send('<h1>About us!</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather forecast')
+    res.send({
+        location: 'Null island',
+        forecast: 'Clear forever. Currently 0 degrees outside. 0% chance of rain.'
+    })
 })
 
 app.listen(3000, () => {
