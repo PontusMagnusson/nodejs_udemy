4a599220f893faffc3044dfbc5ca12c0f0afd816
MAGNUSSON Pontus <pontus.magnusson@protonmail.com>
2019-05-14 09:57:52 +0200

    Finalize callback pattern, add object destructuring, error handling and commandline args

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,46 +1,25 @@
-const request = require('request')
 const geocode = require('./utils/geocode')
-const readKeys = require('./utils/readKeys')
+const forecast = require('./utils/forecast')
 
+const address = process.argv[2]
 
-// Get API keys
-const keyData = readKeys()
-const darkskyApiKey = keyData.darksky
+if (address === undefined) {
+    return console.log('Please provide a valid location')
+} else if (process.argv[3] !== undefined) {
+    return console.log('Please provide only one argument. Names containing spaces should be wrapped in quotes.')
+}
 
-
-// Weather service
-const darkskyUrl = `https://api.darksky.net/forecast/${darkskyApiKey}/37.8267,-122.4233?units=si`
-
-request({url: darkskyUrl, json: true}, (err, response) => {
-    if(err) {
-        console.log('Unable to contact DarkSky weather service, please try again later...')
-    } else if(response.body.error) {
-        console.log(response.body.error)
-    } else {
-        console.log('%s It is currently %f degrees out. There is a %f\% of rain.', response.body.daily.data[0].summary, response.body.currently.temperature, response.body.currently.precipProbability)
+geocode(address, (error, { latitude, longitude, location }) => {
+    if (error) {
+        return console.log(error)
     }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log(location)
+        console.log(forecastData)
+    })
 })
-
-
-// // Geocoding
-// const mapboxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Varberg.json?access_token=${mapboxApiKey}`
-
-// request({url: mapboxUrl, json: true}, (err, response) => {
-
-//     if (err) {
-//         console.log('Unable to contact Mapbox services, please try again later...')
-//     } else if (response.body.features.length === 0) {
-//         console.log('Unable to find location, please try again with a different search term')
-//     } else {
-//         const lat = response.body.features[0].center[1]
-//         const long = response.body.features[0].center[0]
-//         console.log('Lat: ' + lat)
-//         console.log('Long: ' + long)
-//     }
-// })
-
-geocode('Gothenburg usa', (err, data) => {
-    console.log(err)
-    console.log(data)
-})
-
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -2,23 +2,22 @@ const request = require('request')
 const readKeys = require('./readKeys')
 
 
-const keyData = readKeys()
-const mapboxApiKey = keyData.mapbox
+const { mapboxApiKey } = readKeys()
 
 const geocode = (address, callback) => {
     const encodedAddress = encodeURIComponent(address)
-    const mapboxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodedAddress}.json?access_token=${mapboxApiKey}`
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodedAddress}.json?access_token=${mapboxApiKey}`
 
-    request({url: mapboxUrl, json: true}, (err, response) => {
-        if (err) {
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
             callback('Unable to contact location services', undefined)
-        } else if (response.body.features.length === 0) {
+        } else if (body.features.length === 0) {
             callback('Unable to find location, please try again with a different search term', undefined)
         } else {
             callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
             })
         }
     })
