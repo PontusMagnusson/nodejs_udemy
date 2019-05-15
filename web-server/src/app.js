067cbb5b646b19e26ad98404bb2823415b2acba2
MAGNUSSON Pontus <pontus.magnusson@protonmail.com>
2019-05-15 15:13:53 +0200

    Add templates and partials, add styling and favicon

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
+const publicPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Configure static resources
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(publicPath))
 
 app.get('', (req, res) => {
     res.render('index', {
@@ -18,12 +26,14 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About this app',
-        name: 'War kitteh'
+        name: 'Pontus'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
+        title: 'Help!',
+        name: 'Pontus',
         message: 'This is an example message'
     })
 })
@@ -32,6 +42,22 @@ app.get('/weather', (req, res) => {
     res.send({
         location: 'Null island',
         forecast: 'Clear forever. Currently 0 degrees outside. 0% chance of rain.'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'This article does not exist', 
+        name: 'Pontus'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Oops, the page you are looking for does not exist, sorry about that.',
+        name: 'Pontus'
     })
 })
 
