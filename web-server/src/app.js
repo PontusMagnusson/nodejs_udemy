491c5cba718d8c5f38b81046304bda011820254a
MAGNUSSON Pontus <pontus.magnusson@protonmail.com>
2019-05-15 13:28:26 +0200

    Setup handlebars to allow for templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,8 +3,30 @@ const express = require('express')
 
 const app = express()
 
+app.set('view engine', 'hbs')
+
 // Configure static resources
 app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('', (req, res) => {
+    res.render('index', {
+         title: 'Sunny weather', 
+         name: 'Pontus' 
+        })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About this app',
+        name: 'War kitteh'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'This is an example message'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
